5db977e1a97aeb77e45616fe8c70d9e9397b7791
Haider Al-zubaidy <h.alzubaidy98@gmail.com>
2020-09-08 11:30:32 +0100

    Added try catch

--- src/main/java/TimeInRange.java
+++ src/main/java/TimeInRange.java
@@ -1,5 +1,6 @@
 import java.sql.Time;
 import java.time.Instant;
+import java.time.temporal.ChronoField;
 import java.util.Scanner;
 
 public class TimeInRange
@@ -7,22 +8,31 @@ public class TimeInRange
 
     public static boolean isInTimeRange(String timeRange, Instant time)
     {
+
         //Converting time to a string then using substring to get relevant information
-        String timeS = time.toString().substring(time.toString().lastIndexOf("T" )+1,time.toString().lastIndexOf("T" )+3);
+        String timeS = time.toString().substring(time.toString().lastIndexOf("T") + 1, time.toString().lastIndexOf("T") + 3);
         int hours = Integer.parseInt(timeS);
+        int range1 =0;
+        int range2 =0;
 
-        //Splitting the time range into two variables
-        int range1 = Integer.parseInt(timeRange.substring(0,2));
-        int range2 = Integer.parseInt(timeRange.substring(3,5));
+        try
+        {
+            //Splitting the time range into two variables
+             range1 = Integer.parseInt(timeRange.substring(0, 2));
+             range2 = Integer.parseInt(timeRange.substring(3, 5));
+        }
+        catch(IllegalArgumentException exception)
+        {
+            System.out.println("IllegalArgumentException, Please make sure the formatting is valid");
+        }
 
-        //Main logic to compare if in range
-        if((range1 <= hours && !(hours > 23)) || range2 >= hours)
+
+
+        // Main logic for time in range
+        if (range1 <= hours || range2 >= hours) ;
         {
             return true;
         }
-        else
-        {
-            return false;
-        }
+
     }
 }
