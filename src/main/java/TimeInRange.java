0d093ebec9bf59db5925eb0cbda9e768ef457baa
Haider Al-zubaidy <h.alzubaidy98@gmail.com>
2020-09-08 10:22:50 +0100

    Fixed static issue

--- src/main/java/TimeInRange.java
+++ src/main/java/TimeInRange.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class TimeInRange
 {
-    static int range1;
-    static int range2;
+
     public static boolean isInTimeRange(String timeRange, Instant time)
     {
         //Converting time to a string then using substring to get relevant information
@@ -13,8 +12,8 @@ public class TimeInRange
         int hours = Integer.parseInt(timeS);
 
         //Splitting the time range into two variables
-        range1 = Integer.parseInt(timeRange.substring(0,2));
-        range2 = Integer.parseInt(timeRange.substring(3,5));
+        int range1 = Integer.parseInt(timeRange.substring(0,2));
+        int range2 = Integer.parseInt(timeRange.substring(3,5));
 
         //Main logic to compare if in range
         if((range1 <= hours && !(hours > 23)) || range2 >= hours)
--- src/test/java/TimeInRangeTest.java
+++ src/test/java/TimeInRangeTest.java
@@ -13,18 +13,13 @@ public class TimeInRangeTest extends TimeInRange
         Instant now = Instant.now();
         Instant day = Instant.parse(("2020-08-09T10:00:00.00Z"));
         Instant night = Instant.parse(("2020-08-09T02:00:00.00Z"));
-        //Testing Substring Values
-        TimeInRange.isInTimeRange("12-20",now);
-        assertEquals(TimeInRange.range1, 12);
-        assertEquals(TimeInRange.range2 , 20);
 
         //Testing TimeInRange Class
-
         //Testing against current Time
         assertTrue(TimeInRange.isInTimeRange("00-23", now));
         //Testing against custom time
         assertTrue(TimeInRange.isInTimeRange("09-17", day));
-        assertTrue(TimeInRange.isInTimeRange("18-07", night));
+        assertTrue(TimeInRange.isInTimeRange("00-06", night));
 
     }
 }
