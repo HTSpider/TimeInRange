beef055057b6ece86f972632d7262a89a94b11c6
Haider Al-zubaidy <h.alzubaidy98@gmail.com>
2020-09-09 10:25:15 +0100

    Making code more efficient

--- src/main/java/TimeInRange.java
+++ src/main/java/TimeInRange.java
@@ -1,19 +1,16 @@
-import java.sql.Time;
 import java.time.Instant;
+import java.time.ZoneId;
 import java.time.temporal.ChronoField;
-import java.util.Scanner;
 
 public class TimeInRange
 {
-
     public static boolean isInTimeRange(String timeRange, Instant time)
     {
-
-        //Converting time to a string then using substring to get relevant information
-        String timeS = time.toString().substring(time.toString().lastIndexOf("T") + 1, time.toString().lastIndexOf("T") + 3);
-        int hours = Integer.parseInt(timeS);
+        String timeString = time.toString();
         int range1 =0;
         int range2 =0;
+        // getting the hour variable from time
+        int hours = Instant.parse(timeString).atZone(ZoneId.of("UTC")).get((ChronoField.HOUR_OF_DAY));
 
         try
         {
@@ -26,13 +23,10 @@ public class TimeInRange
             System.out.println("IllegalArgumentException, Please make sure the formatting is valid");
         }
 
-
-
         // Main logic for time in range
         if (range1 <= hours || range2 >= hours) ;
         {
             return true;
         }
-
     }
 }
--- src/test/java/TimeInRangeTest.java
+++ src/test/java/TimeInRangeTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import java.sql.Time;
 import java.time.Instant;
+import java.time.ZoneId;
 import java.time.temporal.*;
 
 public class TimeInRangeTest extends TimeInRange
@@ -14,9 +15,11 @@ public class TimeInRangeTest extends TimeInRange
         Instant now = Instant.now();
         Instant day = Instant.parse(("2020-08-09T10:00:00.00Z"));
         Instant night = Instant.parse(("2020-08-09T02:00:00.00Z"));
+
         //Testing TimeInRange Class
         //Testing against current Time
         assertTrue(TimeInRange.isInTimeRange("00-23", now));
+
         //Testing against custom time
         assertTrue(TimeInRange.isInTimeRange("06-17", day));
         assertTrue(TimeInRange.isInTimeRange("00-06", night));
